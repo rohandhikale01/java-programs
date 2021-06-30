e55c8fc847cc94b7e576d2e6f9a391bce2a2cf94
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-07-01 00:24:00 +0530

    Add files via upload

---
+++ assignement_oprt.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class assignement_oprt {
+
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+
+       /* int a,b=5;
+        a=b; //we are assigning the value of b to a //
+        System.out.println("Value of a= "+a); */
+
+        System.out.println("Entre the value of float- ");
+        float f;
+        f=input.nextFloat();
+       // f+=5;  //Evaluate f=f+5;
+        //System.out.println(f); 
+        f*=2;
+        System.out.println(f);
+
+
+    }
+    
+}
