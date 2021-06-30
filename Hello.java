0fd5e1fc8b7f01923a0f7bb88fd0ada1675922d8
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-06-30 10:33:28 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello{
+
+public static void main(String[] args){
+
+  System.out.println("Inside Main Method");
+  sayhi(); //Calling Sayhi Method //
+}
+public static void sayhi() 
+{
+  System.out.println("Inside Sayhi Method");
+
+}
+
+}
