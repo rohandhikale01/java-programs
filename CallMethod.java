d2aaaf3b4aa689a825dbe104d1dfb7b69824036b
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-07-02 23:31:43 +0530

    Add files via upload

---
+++ CallMethod.java
@@ -0,0 +1,11 @@
+public class CallMethod {
+   public static void main(String[] args) {           
+       name(); //Calling method namev// 
+    }
+
+    public static void name(){
+        System.out.println("Name- Rohan");
+        System.out.println("Age-21");
+    }
+    
+}
