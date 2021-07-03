0637edd005b48d24aa3596a6c074fabf61941741
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-07-03 12:02:58 +0530

    Add files via upload

---
+++ data_type_range.java
@@ -0,0 +1,23 @@
+public class data_type_range {
+    public static void main(String[] args)
+    {
+        //We are printing the Range of Different Data types //
+        System.out.print("Range of an integer:");
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+     
+
+        System.out.print("Range of an short:");
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+     
+     
+        System.out.print("Range of an long:");
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+        System.out.print("Range of an Double:");
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+    }  
+}
