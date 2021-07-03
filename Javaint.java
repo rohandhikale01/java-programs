6816d2e963614d1f13ce00d38020930546d57e4f
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-07-03 12:03:41 +0530

    Add files via upload

---
+++ Javaint.java
@@ -0,0 +1,10 @@
+public class Javaint {
+	public static void main(String[] args) {
+	
+	int i=0;
+	int j=100;
+	
+	System.out.println("Value of int i is: "+i);
+	System.out.println("Value of int j is: "+j);
+	}
+}
