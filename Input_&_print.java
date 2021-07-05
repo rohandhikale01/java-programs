bbbbe188e4e354fab3fe2f23d38158e3799b5f32
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-07-05 22:41:18 +0530

    Add files via upload

---
+++ Input_&_print.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+  public class Input_&_print {
+
+    public static void main(String[] args)
+    {   
+        Scanner input =new Scanner(System.in);
+        System.out.println("Entre your Roll: ");
+        int r;
+        r=s.nextInt();
+        System.out.println("Roll number- " +r);
+    }
+    
+}
