b9bc3576b33c65fce9953bb42f99b400b8b6ddd1
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-07-10 17:51:13 +0530

    Add files via upload

---
+++ BitWise.java
@@ -0,0 +1,14 @@
+public class BitWise {
+
+ public static void main(String[] args) {
+        
+    int a=20; //1 0 1 0 0
+    int b=15; //0 1 1 1 1//
+    int c=a&b;
+    System.out.println("Bitwise & = " +c);
+
+    c=a|b;
+    System.out.println("Bitwise or = "+c);
+    }
+    
+}
