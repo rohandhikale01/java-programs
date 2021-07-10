32974ab7d16009596dca8f214672ba8681cbdb49
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-07-10 17:46:31 +0530

    Add files via upload

---
+++ binary.java
@@ -0,0 +1,12 @@
+
+public class binary {
+    
+     public static void main(String a[])
+      {
+          int i=0b101; //adding 0b or 0B before binary number you can convert it into int //
+          int j=0b10_00_10_1; //this underscore is usefull to calculate zero's //
+
+        System.out.println(i);
+        System.out.println(j);
+}
+}
