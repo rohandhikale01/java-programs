714f8abc5dad2e6ff2f3a9b364b75036f6dd3f33
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-07-15 00:28:29 +0530

    Add files via upload

---
+++ StaticBlock_Demo.java
@@ -0,0 +1,13 @@
+
+public class StaticBlock_Demo {
+
+    static
+    {
+        System.out.println("Inside Static Block"); //JVM Calls this Block Before Main Method//
+    }
+    
+    public static void main(String a[])
+    {
+        
+    }
+}
