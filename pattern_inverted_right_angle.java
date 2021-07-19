09976af456777c988dd6f29a7184650e65200cad
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-07-20 00:28:57 +0530

    Add files via upload

---
+++ pattern_inverted_right_angle.java
@@ -0,0 +1,18 @@
+public class pattern_inverted_right_angle {
+
+ public static void main(String[] args) {
+
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=5-i+1;j++)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+        
+    }
+
+
+    
+}
