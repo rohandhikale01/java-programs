59d7e41de144307ad2e19090e14a5e72c7ffc38c
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-07-20 12:33:13 +0530

    Add files via upload

---
+++ Inverted_traingle_pattern.java
@@ -0,0 +1,23 @@
+public class Inverted_traingle_pattern {
+
+public static void main(String[] args) {
+
+    int i,j,k;
+
+    for(i=1;i<=5;i++)
+    {
+        for(k=1;k<=i;k++)
+        {
+            System.out.print("  ");
+        }
+        for(j=1;j<=(11-(2*i));j++)
+        {
+            System.out.print("* ");
+        }
+
+        System.out.println();
+    }
+        
+    } 
+    
+}
