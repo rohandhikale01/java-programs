466141c5fd26aa9e39caf5b20a8392776543efad
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-09-17 23:31:05 +0530

    Add files via upload

---
+++ toLowercase.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class toLowercase {
+
+    public static void main(String[] args) {
+        
+        Scanner scan=new  Scanner(System.in);
+
+       System.out.print("Enter String:  ");
+       String str=scan.nextLine();
+    
+       scan.close();
+
+       char ch[]=new char[str.length()];
+       int temp=0;
+
+       for(int i=0;i<str.length();i++)
+       {
+           temp=str.charAt(i);
+
+           if(temp>=64&&temp<=90)
+           {
+            ch[i]=(char)(temp+32);
+           }
+           else
+           ch[i]=str.charAt(i);    
+       }
+
+       String newstr=String.valueOf(ch);
+
+       System.out.println("Your String in Lower case is: "+newstr);
+        
+            
+    }
+    
+}
