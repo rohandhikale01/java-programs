f2b7a1668af9d732490b77bfd0a18d5c8955f579
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2022-03-17 08:36:51 +0530

    Add files via upload

---
+++ BlackCell.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BlackCell {
+    public static void main(String[] args) {
+        
+        Scanner scan=new Scanner(System.in);
+
+        int cell=scan.nextInt();
+
+        System.out.println(cell*(cell/2));
+    }
+}
