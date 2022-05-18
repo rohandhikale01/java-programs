8b83c62e71ac3f70a7d80435267997a5ca0c94c8
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2022-05-18 23:24:28 +0530

    Add files via upload

---
+++ AorB.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class AorB {
+    public static void main(String[] args) {
+        
+        Scanner scan=new Scanner(System.in);
+        int size=scan.nextInt();
+        int arr[][]=new int[size][2];
+
+        for(int i=0;i<size;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                arr[i][j]=scan.nextInt();
+            }
+        }
+
+        for (int[] is : arr) {
+            getBest(is);
+        }
+    }
+
+    public static void getBest(int arr[])
+    {
+            int count1=0,count2=0;
+
+            count1=(500-2*arr[0])+(1000-(arr[0]+arr[1])*4);
+            count2=(1000-4*arr[1])+(500-(arr[0]+arr[1])*2);
+            if(count1>count2)
+            System.out.println(count1);
+            else
+            System.out.println(count2);
+
+    }
+}
