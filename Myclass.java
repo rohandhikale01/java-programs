60c048b49b5f7faa897c1066cf2caff45868c574
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2022-06-01 21:44:17 +0530

    Add files via upload

---
+++ Myclass.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Myclass {
+    
+    public static void main(String[] args) {
+        
+        Scanner scan=new Scanner(System.in);
+
+        int num1=scan.nextInt();
+        int num2=scan.nextInt();
+
+        if(num1<num2)
+        System.out.println("YES");
+        else
+        System.out.println("'NO");
+
+    }
+}
+
+
