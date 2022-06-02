16d037434cdf9dfad066587da9a0631f64962de6
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2022-06-02 22:42:41 +0530

    Add files via upload

---
+++ codechef.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class codechef {
+    public static void main(String[] args) {
+        
+        Scanner scan=new Scanner(System.in);
+        int num1=scan.nextInt();
+        int num2=scan.nextInt();
+        int num3=scan.nextInt();
+        int temp=num1-num2;
+        System.out.println(temp+" "+(temp-num3));
+
+    }
+}
