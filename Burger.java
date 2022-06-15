e5a161556f648c05771622cf5d015488cc549da8
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2022-06-15 20:19:58 +0530

    Add files via upload

---
+++ Burger.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+//code chef
+public class Burger {
+    public static void main(String[] args) {
+        
+        Scanner scan=new Scanner(System.in);
+
+        int size=scan.nextInt();
+        int arr[][]=new int[size][2];
+        for(int i=0;i<size;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                arr[i][j]=scan.nextInt();
+            }
+        }
+
+        for (int[] is : arr) {
+            getMin(is);
+        }
+    }
+
+    public static void getMin(int arr[])
+    {
+        if(arr[0]<arr[1])
+        System.out.println(arr[0]);
+        else
+        System.out.println(arr[1]);
+    }
+}
