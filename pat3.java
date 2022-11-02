d875a3c7dae7491878893bbd7d4061257a2996b4
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2022-11-03 00:08:44 +0530

    Add files via upload

--- pat3.java
+++ pat3.java
@@ -1,23 +1,37 @@
 public class pat3 {
     public static void main(String[] args) {
-        
+        int count=3;
         for(int i=0;i<5;i++)
-        {
-            int temp=65;
-            for(int j=5;j>=0;j--)
+        {  
+            for(int k=0;k<2;k++)
             {
+                for(int j=4;j>=0;j--)
+                {
                 if(i>=j)
                 {
-                System.out.print((char)(temp)+" ");
-                temp++;
+                System.out.print("* ");
                     
                 }
                 else
                 System.out.print(" ");
+                }
 
+                if(k==0)
+                {
+                    for(int l=0;l<count;l++)
+                    {
+                    System.out.print(" ");
 
+                    }
+                }
             }
             System.out.println();
+            count--;        
+            }
         }
-    }
-}
+           
+        }
+    
+
+ 
+
